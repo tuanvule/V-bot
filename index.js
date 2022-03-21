4ce54620ed27e26f9ff274f4a456e8b6f2db87c0
letuanvu <ngovipmo1@gmail.com>
2022-03-21 19:56:23 +0700

    First commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { Client, Intents } = require('discord.js')
 const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} )
 
-client.login(process.env.token)
+const token = 'OTU1MDUyNDMzNjk0OTQ5NDM3.YjcD5A.fUIZa-NsLpPVe1aD1Rtok3ExypA'
+
+client.login(token)
 client.on('ready', () => {
-    console.log(client.user.tag + ' đã online')
+    console.log(client.user.tag + ' đã online' )
 })
 
 client.on('message', message => {
 
     if(message.content === 'ping') {
-        message.channel.send({content: 'bớt ping đi'})
-    }
+        message.channel.send({content: 'ping cài đb'})
+    }    
 })
