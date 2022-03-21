4f26a77546b777a1806b97852ac2fb0c843ec9ae
letuanvu <ngovipmo1@gmail.com>
2022-03-21 18:43:35 +0700

    Add existing file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Intents } = require('discord.js')
 const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} )
 
-client.login(process.env.token)
+client.login('OTU1MDUyNDMzNjk0OTQ5NDM3.YjcD5A.brBJUlesxkEhONePl0S63iDv81Q')
 client.on('ready', () => {
     console.log(client.user.tag + ' đã online')
 })
