fd7d7894ed1b7d814e7a399bd2eb32af5772f937
letuanvu <ngovipmo1@gmail.com>
2022-03-21 18:33:50 +0700

    Add existing file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, Intents } = require('discord.js')
-const { token } = require('./config.json')
 const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} )
 
 client.login(process.env.token)
