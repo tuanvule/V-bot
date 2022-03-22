288d1035823db2b1a31f7ccc45e79f626d00d1c7
letuanvu <ngovipmo1@gmail.com>
2022-03-22 17:36:00 +0700

    a

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD
 
 // const token = 'OTU1MDUyNDMzNjk0OTQ5NDM3.YjcD5A.bfBXJUQgybnMCUSrWUmWpee7b7g'
 
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-  token: process.env.token,
-});
-
 client.login(process.env.token)
 client.on('ready', () => {
     console.log(client.user.tag + ' đã online' )
