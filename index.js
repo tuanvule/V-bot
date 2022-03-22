7226abefd6b0bce9f65b1e45812cd336c189e382
letuanvu <ngovipmo1@gmail.com>
2022-03-22 15:52:47 +0700

    new file

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { Client, Intents } = require('discord.js')
 const client = new Client( { intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} )
 
-const token = 'OTU1MDUyNDMzNjk0OTQ5NDM3.YjcD5A.fUIZa-NsLpPVe1aD1Rtok3ExypA'
+// const token = 'OTU1MDUyNDMzNjk0OTQ5NDM3.YjcD5A.bfBXJUQgybnMCUSrWUmWpee7b7g'
 
-client.login(token)
+const aws = require('aws-sdk');
+
+let s3 = new aws.S3({
+  token: process.env.token,
+});
+
+client.login(process.env.token)
 client.on('ready', () => {
     console.log(client.user.tag + ' đã online' )
 })
